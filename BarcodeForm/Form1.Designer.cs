47099b97120e6458ca1561406147efba79e95c9b
Renzelle <70456007+Renzelle@users.noreply.github.com>
2025-05-10 16:37:22 +0800

    Add files via upload

---
+++ BarcodeForm/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace BarcodeForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.buttonDownload = new System.Windows.Forms.Button();
+            this.comboBoxFonts = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(442, 81);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(489, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(567, 536);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.Location = new System.Drawing.Point(171, 223);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(129, 51);
+            this.buttonPrint.TabIndex = 3;
+            this.buttonPrint.Text = "Print";
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click_1);
+            // 
+            // buttonDownload
+            // 
+            this.buttonDownload.Location = new System.Drawing.Point(333, 223);
+            this.buttonDownload.Name = "buttonDownload";
+            this.buttonDownload.Size = new System.Drawing.Size(129, 51);
+            this.buttonDownload.TabIndex = 4;
+            this.buttonDownload.Text = "Download As PDF";
+            this.buttonDownload.UseVisualStyleBackColor = true;
+            this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click_1);
+            // 
+            // comboBoxFonts
+            // 
+            this.comboBoxFonts.FormattingEnabled = true;
+            this.comboBoxFonts.Location = new System.Drawing.Point(94, 426);
+            this.comboBoxFonts.Name = "comboBoxFonts";
+            this.comboBoxFonts.Size = new System.Drawing.Size(150, 21);
+            this.comboBoxFonts.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 429);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Select Font:";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1105, 602);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxFonts);
+            this.Controls.Add(this.buttonDownload);
+            this.Controls.Add(this.buttonPrint);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.ComboBox comboBoxFonts;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BarcodeForm/Form1.cs
@@ -0,0 +1,231 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+using System.Drawing.Printing;
+using System.Drawing.Imaging;
+using System.IO;
+using OfficeOpenXml;  // EPPlus for Excel
+using OfficeOpenXml.Drawing;
+
+namespace BarcodeForm
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "server=localhost;database=barcode;user=root;password='';";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                string query = "SELECT * FROM masterfile";
+                DataTable dt = new DataTable();
+
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dt);
+                    }
+                }
+
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+
+                string barcodeData = selectedRow.Cells["Barcode"].Value.ToString().PadLeft(12, '0'); // Ensure 12 digits for EAN-13
+                string topText = "" + selectedRow.Cells["BranchDesc"].Value.ToString();
+                string unitPrice = selectedRow.Cells["UnitPrice"].Value.ToString();
+                string formattedPrice = unitPrice.Replace(",", " , ").Replace(".", " . ");
+                string bottomText = "P" + formattedPrice;
+
+                string leftBottomText = "" + selectedRow.Cells["Model"].Value.ToString();
+
+                Bitmap barcodeImage = GenerateBarcode(barcodeData, topText, bottomText, leftBottomText);
+                if (barcodeImage != null)
+                {
+                    pictureBox1.Image = barcodeImage;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a row from the data grid.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (FontFamily font in System.Drawing.FontFamily.Families)
+            {
+                comboBoxFonts.Items.Add(font.Name);
+            }
+            comboBoxFonts.SelectedIndex = 0; // Set default font
+            LoadData();
+        }
+
+        private Bitmap GenerateBarcode(string data, string topText, string bottomText, string leftBottomText)
+        {
+            if (data.Length != 12)
+            {
+                MessageBox.Show("EAN-13 requires 12 digits.", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return null;
+            }
+
+            string selectedFontName = comboBoxFonts.SelectedItem?.ToString() ?? "Arial"; // Default to Arial
+
+            int barcodeWidth = 400;
+            int barcodeHeight = 50;
+
+            BarcodeWriter barcodeWriter = new BarcodeWriter
+            {
+                Format = BarcodeFormat.EAN_13,
+                Options = new ZXing.Common.EncodingOptions
+                {
+                    Width = barcodeWidth,
+                    Height = barcodeHeight,
+                    Margin = 10,
+                    PureBarcode = true
+                },
+                Renderer = new ZXing.Rendering.BitmapRenderer
+                {
+                    Foreground = Color.Black,
+                    Background = Color.White
+                }
+            };
+
+            Bitmap barcodeBitmap = barcodeWriter.Write(data);
+
+            Bitmap finalBitmap = new Bitmap(barcodeWidth + 20, barcodeHeight + 100, PixelFormat.Format32bppArgb);
+
+            using (Graphics g = Graphics.FromImage(finalBitmap))
+            {
+                g.Clear(Color.White);
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+
+                g.DrawImage(barcodeBitmap, 2, 30, barcodeWidth, barcodeHeight);
+
+                using (Font font = new Font(selectedFontName, 18, FontStyle.Bold))
+                using (Font modelFont = new Font(selectedFontName, 16, FontStyle.Regular))
+                using (Font barcodeFont = new Font(selectedFontName, 18, FontStyle.Regular))
+                using (SolidBrush brush = new SolidBrush(Color.Black))
+                {
+                    //Top Text
+                    SizeF topTextSize = g.MeasureString(topText, font);
+                    g.DrawString(topText, font, brush, (finalBitmap.Width - topTextSize.Width) / 2, 1);
+                    //Center Bottom Text
+                    SizeF bottomTextSize = g.MeasureString(bottomText, font);
+                    g.DrawString(bottomText, font, brush, (finalBitmap.Width - bottomTextSize.Width) / 2, finalBitmap.Height - 35);
+                    //Left Bottom Text
+                    g.DrawString(leftBottomText, modelFont, brush, 35, finalBitmap.Height - 50);
+                    //Center Barcode Text
+                    string formattedBarcode = $"{data[0]} {data.Substring(1, 6)} {data.Substring(7, 5)} {data[11]}";
+                    SizeF barcodeTextSize = g.MeasureString(formattedBarcode, barcodeFont);
+                    g.DrawString(formattedBarcode, barcodeFont, brush, (finalBitmap.Width - barcodeTextSize.Width) / 2, barcodeHeight + 27);
+                }
+            }
+
+            return finalBitmap;
+        }
+
+        private void PrintDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+
+                int dpi = 30; // High DPI
+                int printWidth = (int)(pictureBox1.Image.Width * (dpi / 96.0));
+                int printHeight = (int)(pictureBox1.Image.Height * (dpi / 46.0));
+
+                e.Graphics.DrawImage(pictureBox1.Image, new Rectangle(0, 0, printWidth, printHeight));
+            }
+        }
+
+        private void buttonPrint_Click_1(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                PrintDocument printDocument = new PrintDocument();
+                printDocument.PrintPage += new PrintPageEventHandler(PrintDocument_PrintPage);
+
+                PrintDialog printDialog = new PrintDialog();
+                printDialog.Document = printDocument;
+
+                if (printDialog.ShowDialog() == DialogResult.OK)
+                {
+                    printDocument.Print();
+                }
+            }
+            else
+            {
+                MessageBox.Show("No barcode to print!", "Print Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonDownload_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("No data to export.", "Export Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string folderPath = "Barcodes"; // Folder where barcodes will be saved
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            foreach (DataGridViewRow gridRow in dataGridView1.Rows)
+            {
+                if (!gridRow.IsNewRow) // Ignore empty new rows
+                {
+                    string barcodeData = gridRow.Cells["Barcode"].Value.ToString().PadLeft(12, '0');
+                    string branch = gridRow.Cells["BranchDesc"].Value.ToString();
+                    string model = gridRow.Cells["Model"].Value.ToString();
+                    string price = "P" + gridRow.Cells["UnitPrice"].Value.ToString();
+
+                    // Generate barcode image
+                    Bitmap barcodeImage = GenerateBarcode(barcodeData, branch, price, model);
+                    if (barcodeImage != null)
+                    {
+                        string imagePath = Path.Combine(folderPath, $"{barcodeData}.png");
+                        barcodeImage.Save(imagePath, ImageFormat.Png);
+                    }
+                }
+            }
+
+            MessageBox.Show("All barcode images have been saved!", "Download Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Open the folder automatically
+            System.Diagnostics.Process.Start("explorer.exe", folderPath);
+        }
+    }
+}
